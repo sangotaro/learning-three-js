5f6c37374453de4e90e3383e8504f00d82b5ed58
Shotaro Watanabe <shotaro.watanabe0715@gmail.com>
2021-02-22 18:14:05 +0900

    feat: three.js sample (#1)

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Learning three.js</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Link href="/chapter-01">
+        <a>Chapter 01</a>
+      </Link>
+    </div>
+  )
+}
